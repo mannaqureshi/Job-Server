fa61c833335911c41ffd0aad94d5516b916acab1
mannaqureshi <mannaqureshi@gmail.com>
2020-03-16 19:24:45 +0500

    "added ssl"

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 
+var sslRedirect = require("heroku-ssl-redirect");
 const storage = path.join(__dirname, "tokens.json");
 
 const { getAccessTokenFromCode } = require("./resources/utils");
 require("dotenv").config();
 
 app.use(require("cors")());
+app.use(sslRedirect());
 app.use(require("body-parser").urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api", require("./routes"));
 
-app.listen(process.env.PORT, async () => {
-  console.log("Server Started");
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, async () => {
+  console.log("Server Started");
   try {
     // const logistiveCredentials = await getAccessTokenFromCode("logistive");
     const movonicsCredentials = await getAccessTokenFromCode("movonics");
