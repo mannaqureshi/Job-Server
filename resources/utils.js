0bebdb6488bc7060cff9c8e0e5577925e12a14ca
mannaqureshi <mannaqureshi@gmail.com>
2020-04-12 06:28:20 +0500

    added logistive unbounce

--- resources/utils.js
+++ resources/utils.js
@@ -6,38 +6,6 @@ const path = require("path");
 const crypto = require("crypto");
 const storage = path.join(__dirname, "../", "tokens.json");
 
-module.exports.getAccessTokenFromCode = (company) => {
-  let id = process.env[`${company}_client_id`];
-  let secret = process.env[`${company}_client_secret`];
-  let code = process.env[`${company}_code`];
-  let scope = process.env.scope;
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await axios.post(
-        urls.AccessTokenUrl,
-        qs.stringify({
-          scope,
-          client_id: id,
-          client_secret: secret,
-          code,
-          grant_type: "authorization_code",
-        }),
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-      if (response.data.error) {
-        reject(response.data.error);
-      }
-      resolve(response.data);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
-
 module.exports.refreshAccessToken = (company) => {
   const credentials = JSON.parse(fs.readFileSync(storage));
   let id = process.env[`${company}_client_id`];
@@ -47,7 +15,7 @@ module.exports.refreshAccessToken = (company) => {
     try {
       const response = await axios.post(urls.RefreshTokenUrl, null, {
         params: {
-          refresh_token: credentials.movonics.refresh_token,
+          refresh_token: credentials[company].refresh_token,
           client_id: id,
           client_secret: secret,
           grant_type: `refresh_token`,
@@ -62,3 +30,35 @@ module.exports.refreshAccessToken = (company) => {
     }
   });
 };
+
+// module.exports.getAccessTokenFromCode = (company) => {
+//   let id = process.env[`${company}_client_id`];
+//   let secret = process.env[`${company}_client_secret`];
+//   let code = process.env[`${company}_code`];
+//   let scope = process.env.scope;
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const response = await axios.post(
+//         urls.AccessTokenUrl,
+//         qs.stringify({
+//           scope,
+//           client_id: id,
+//           client_secret: secret,
+//           code,
+//           grant_type: "authorization_code",
+//         }),
+//         {
+//           headers: {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//           },
+//         }
+//       );
+//       if (response.data.error) {
+//         reject(response.data.error);
+//       }
+//       resolve(response.data);
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+// };
--- routes.js
+++ routes.js
@@ -92,6 +92,41 @@ router.post("/movonics/leads", async (req, res) => {
   }
 });
 
+router.post("/logistive/unbounce", async (req, res) => {
+  try {
+    const { token_type, access_token } = await refreshAccessToken("logistive");
+    console.log(access_token);
+    delete req.body.pageVariant;
+    delete req.body.pageId;
+    console.log(req.body);
+
+    const response = await axios.post(
+      urls.AddLeadUrl,
+      {
+        data: [
+          {
+            ...req.body,
+            Lead_Status: "New Lead",
+            Lead_Source: "Movonics_UB",
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `${token_type} ${access_token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    const repsonseMessage =
+      response.data.data[0].code == "SUCCESS" ? true : false;
+    return res.status(200).redirect(`https://www.movonics.com/thankyou`);
+  } catch (error) {
+    console.log(error);
+    return res.status(302).redirect(`https://www.movonics.com/Error`);
+  }
+});
+
 router.post("/movonics/unbounce", async (req, res) => {
   try {
     const { token_type, access_token } = await refreshAccessToken("movonics");
@@ -128,125 +163,125 @@ router.post("/movonics/unbounce", async (req, res) => {
   }
 });
 
-// router.get("/invoices/:id", async (req, res) => {
-//   const invoiceId = req.params.id;
-//   const { token_type, access_token } = await refreshAccessToken("movonics");
-//   try {
-//     const response = await axios.get(`${urls.GetInvoice}/${invoiceId}`, {
-//       headers: {
-//         Authorization: `${token_type} ${access_token}`,
-//       },
-//     });
+router.get("/invoices/:id", async (req, res) => {
+  const invoiceId = req.params.id;
+  const { token_type, access_token } = await refreshAccessToken("movonics");
+  try {
+    const response = await axios.get(`${urls.GetInvoice}/${invoiceId}`, {
+      headers: {
+        Authorization: `${token_type} ${access_token}`,
+      },
+    });
 
-//     if (response.data.code == 0) {
-//       let {
-//         invoice_id,
-//         invoice_number,
-//         invoice_url,
-//         currency_symbol,
-//         line_items,
-//         total,
-//       } = response.data.invoice;
+    if (response.data.code == 0) {
+      let {
+        invoice_id,
+        invoice_number,
+        invoice_url,
+        currency_symbol,
+        line_items,
+        total,
+      } = response.data.invoice;
 
-//       const items = line_items.map(
-//         ({ name, description, rate, quantity, item_total }) => {
-//           return {
-//             name,
-//             description,
-//             rate,
-//             quantity,
-//             total: item_total,
-//           };
-//         }
-//       );
-//       return res.json({
-//         invoice: {
-//           invoice_id,
-//           invoice_number,
-//           invoice_url,
-//           currency_symbol,
-//           items,
-//           total,
-//         },
-//         success: true,
-//       });
-//     }
-//     throw new Error(response.message);
-//   } catch (error) {
-//     return res.json({ message: error, success: false });
-//   }
-// });
+      const items = line_items.map(
+        ({ name, description, rate, quantity, item_total }) => {
+          return {
+            name,
+            description,
+            rate,
+            quantity,
+            total: item_total,
+          };
+        }
+      );
+      return res.json({
+        invoice: {
+          invoice_id,
+          invoice_number,
+          invoice_url,
+          currency_symbol,
+          items,
+          total,
+        },
+        success: true,
+      });
+    }
+    throw new Error(response.message);
+  } catch (error) {
+    return res.json({ message: error, success: false });
+  }
+});
 
-// router.post("/payments/initiate", async (req, res) => {
-//   const { invoice_id, invoice_number } = req.body;
-//   let amount = 0;
-//   const { token_type, access_token } = await refreshAccessToken("movonics");
-//   try {
-//     const response_invoice = await axios.get(
-//       `${urls.GetInvoice}/${invoice_id}`,
-//       {
-//         headers: {
-//           Authorization: `${token_type} ${access_token}`,
-//         },
-//       }
-//     );
-//     amount = `${response_invoice.data.invoice.total}.0`;
+router.post("/payments/initiate", async (req, res) => {
+  const { invoice_id, invoice_number } = req.body;
+  let amount = 0;
+  const { token_type, access_token } = await refreshAccessToken("movonics");
+  try {
+    const response_invoice = await axios.get(
+      `${urls.GetInvoice}/${invoice_id}`,
+      {
+        headers: {
+          Authorization: `${token_type} ${access_token}`,
+        },
+      }
+    );
+    amount = `${response_invoice.data.invoice.total}.0`;
 
-//     const params = {
-//       amount,
-//       autoRedirect: 0,
-//       orderRefNum: invoice_number,
-//       // paymentMethod: "CC_PAYMENT_METHOD",
-//       postBackURL: `http://localhost:5000/api/payments/firstHandler`,
-//       storeId: "9813",
-//     };
+    const params = {
+      amount,
+      autoRedirect: 0,
+      orderRefNum: invoice_number,
+      // paymentMethod: "CC_PAYMENT_METHOD",
+      postBackURL: `http://localhost:5000/api/payments/firstHandler`,
+      storeId: "9813",
+    };
 
-//     let queryParams = Object.keys(params).reduce((acc, key) => {
-//       return (acc += `${key}=${params[key]}&`);
-//     }, ``);
-//     queryParams = queryParams.substr(0, queryParams.length - 1);
-//     console.log(queryParams);
-//     console.log(
-//       aes.encText(queryParams, `LGPL1103O1MJ947U`, null, "base64", "base64")
-//     );
+    let queryParams = Object.keys(params).reduce((acc, key) => {
+      return (acc += `${key}=${params[key]}&`);
+    }, ``);
+    queryParams = queryParams.substr(0, queryParams.length - 1);
+    console.log(queryParams);
+    console.log(
+      aes.encText(queryParams, `LGPL1103O1MJ947U`, null, "base64", "base64")
+    );
 
-//     // 2077566000001660111
+    // 2077566000001660111
 
-//     const response = await axios.post(
-//       `https://easypay.easypaisa.com.pk/easypay/Index.jsf`,
-//       qs.stringify({
-//         ...params,
-//         merchantHashedReq: aes.encText(
-//           queryParams,
-//           `LGPL1103O1MJ947U`,
-//           null,
-//           "base64",
-//           "base64"
-//         ),
-//       }),
-//       {
-//         headers: {
-//           "Content-Type": "application/x-www-form-urlencoded",
-//         },
-//       }
-//     );
-//     console.log(response.data);
+    const response = await axios.post(
+      `https://easypay.easypaisa.com.pk/easypay/Index.jsf`,
+      qs.stringify({
+        ...params,
+        merchantHashedReq: aes.encText(
+          queryParams,
+          `LGPL1103O1MJ947U`,
+          null,
+          "base64",
+          "base64"
+        ),
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    console.log(response.data);
 
-//     res.json({ ...response.data });
-//   } catch (error) {
-//     console.log("Error");
-//     return res.json({ message: error, success: false });
-//   }
-// });
+    res.json({ ...response.data });
+  } catch (error) {
+    console.log("Error");
+    return res.json({ message: error, success: false });
+  }
+});
 
-// router.get("/payments/firstHandler", (req, res) => {
-//   const { auth_token, postBackURL } = req.query;
-//   res.json({ success: true, auth_token, postBackURL });
-// });
+router.get("/payments/firstHandler", (req, res) => {
+  const { auth_token, postBackURL } = req.query;
+  res.json({ success: true, auth_token, postBackURL });
+});
 
-// router.get("/payments/secondHandler", (req, res) => {
-//   const { status, desc, orderRefNumber } = req.query;
-//   res.json({ status, desc, orderRefNumber });
-// });
+router.get("/payments/secondHandler", (req, res) => {
+  const { status, desc, orderRefNumber } = req.query;
+  res.json({ status, desc, orderRefNumber });
+});
 
 module.exports = router;
